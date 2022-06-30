389a746748edf815fd4764b519337c7537636e89
luizalinhares <100442678+luizalinhares@users.noreply.github.com>
2022-06-30 01:06:37 -0300

    Add files via upload

---
+++ lista de tarefas/js/control.js
@@ -0,0 +1,72 @@
+let contador = 0;
+let input = document.getElementById('inputTarefa');
+let btnAdd = document.getElementById('btn-add');
+let main = document.getElementById('areaLista');
+
+
+function addTarefa() {
+ //pegar o valor digitado no input
+  let valorInput = input.value;
+// se nao for vazio, nem nulo, nem indefindio
+  if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
+    
+    ++contador;
+
+    let novoItem = `<div id="${contador}" class="item">
+  <div onclick="marcarTarefa(${contador})"  class="item-icone">
+    <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+  </div>
+  <div onclick="marcarTarefa(${contador})"  class="item-nome">
+    ${valorInput}
+  </div>
+  <div class="item-botao">
+    <button onclick="deletar(${contador})" class="delete">DELETE<i class="mdi mdi-delete"></i></button>
+  </div>
+</div>`;
+    //ADICIONAR NOVO ITEM NO MAIN
+    main.innerHTML += novoItem;
+    //ZERAR OS CAMPOS
+    input.value = "";
+    input.focus();
+
+
+
+  }
+
+}
+
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+}
+
+function marcarTarefa(id){
+  var item = document.getElementById(id);
+  var classe = item.getAttribute('class');
+  console.log(classe);
+
+  if (classe == "item") {
+    item.classList.add('clicado');
+ 
+    var icone = document.getElementById('icone_' + id);
+    icone.classList.remove('mdi-circle-outline');
+    icone.classList.add('mdi-check-circle');
+  }else{
+    item.classList.remove('clicado');
+ 
+    var icone = document.getElementById('icone_' + id);
+    icone.classList.remove('mdi-check-circle');
+    icone.classList.add('mdi-circle-outline');
+  }
+
+
+
+}
+
+input.addEventListener("keyup", function(event){
+//se teclou enter (tecla número 13 no teclado)
+if (event.keyCode === 13){
+  event.preventDefault();
+  btnAdd.click();
+}
+})
